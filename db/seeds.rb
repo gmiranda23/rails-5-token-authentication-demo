10ba7a10c2ed078e3f4d0757b46c3d9fa9c38be8
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2015-02-24 12:26:00 -0300

    Add demonstration user to database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+password = SecureRandom.base64
+User.where(email: 'alice@example.com').first_or_create(password: password, password_confirmation: password, authentication_token: '1G8_s7P-V-4MGojaKD7a')
