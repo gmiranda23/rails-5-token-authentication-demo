0308965d910d69c8c10680e40302edd3e0f71706
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2015-02-24 12:27:43 -0300

    Add HomeController#restricted_index which requires authentication
    Authentication is now required. Token authentication can be used
    by users to authenticate themselves.

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
 
+  get '/restricted_index' => 'home#restricted_index'
+
   root 'home#index'
 end
